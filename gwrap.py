3709b321825f254e2705eb22ea6254ce6a254a4c
Sidd Ghosh <siddsghosh@gmail.com>
2019-10-25 16:24:47 -0600

    Added parsing time period processing

--- gwrap.py
+++ gwrap.py
@@ -20,8 +20,22 @@ def getUlist( usrstr, vartype ):
     return usrs
 
 def procPeriod( perstr ):
+    from datetime import datetime
     if perstr == None:
        return []
+    lst = perstr.split('-')
+    mn = 1; day = 1;
+    lstn = []
+    for e in lst:
+       if len(e) > 0:
+          yr = int(e[:4])
+          if len(e) > 4:
+             mn = int(e[4:6])
+             if len(e) > 6:
+                day = int(e[6:8])
+          en = datetime(yr, mn, day).timestamp()
+          lstn.append(en)
+    return lstn
 
 
 
@@ -40,8 +54,8 @@ grprprtby.add_argument('--cache-mode', dest='cmode', action='store_true',
                        help='Generate cache for grprt')
 grprprtby.add_argument('--list-mode=', dest='lmode',choices=['name','size','owner','project',\
                        'mtime','atime'], 
-                       help='Generate list of files with one or more of these attributes in space\
-                       separated format')
+                       help='Generate list of files with one or more of these attributes in order\
+                       as specified in command line argument in space separated format')
 
 parser.add_argument('--unames=', dest='fuids', nargs='+', metavar=('User1','User2'),
                     help='for content owned only by users User1, User2..')
@@ -49,12 +63,12 @@ parser.add_argument('--projects=', dest='projs', nargs='+', metavar=('Proj1','Pr
                     help='for content assocaited with projects Proj1, Proj2.., applicable\
                           only in HPSS')
 parser.add_argument('--write-period=', dest='writep', metavar='YYYY[MM[DD]]-YYYY[MM[DD]]',
-                    help='for content written during time window YYYY[MM[DD]]-YYYY[MM[DD]] \
+                    help='for content written during the time window YYYY[MM[DD]]-YYYY[MM[DD]] \
                           either of begin or end period may be omitted for open interval but\
                           not both. For time specification 4-digit year is required, 2-digit\
                           month may be specified, if month is specified 2-digit may be specified.')
 parser.add_argument('--read-period=', dest='readp',  metavar='YYYY[MM[DD]]-YYYY[MM[DD]]',
-                    help='for content read during time window YYYY[MM[DD]]-YYYY[MM[DD]] \
+                    help='for content read during time the window YYYY[MM[DD]]-YYYY[MM[DD]] \
                           either of begin or end period may be omitted for open interval but\
                           not both. For time specification 4-digit year is required, 2-digit\
                           month may be specified, if month is specified 2-digit may be specified.')
@@ -69,7 +83,9 @@ args = parser.parse_args()
 cachedir = args.cdir
 users = getUlist( args.fuids, 'users' )
 projects = getUlist( args.projs, 'projects' )
-#writep = procPeriod( writep )
+writep = procPeriod( args.writep )
+readp = procPeriod( args.readp )
 print(cachedir)
 print(users)
 print(projects)
+print(writep)
