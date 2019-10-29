4265c9226f0833549e934fea5e7912e26abe0726
Siddhartha Ghosh <sghosh@ucar.edu>
2019-10-29 11:40:46 -0600

    Querygen script is complete, I will just add little more documentation in the comment field

--- querygen.py
+++ querygen.py
@@ -1,30 +1,43 @@
-def getQryStr( usrs, projs, wp, rp, prefname, fields ):
+def usrProjPart( entity, ups ):
+    tmp = list(map(lambda x: entity + ' = ' + str(x), ups))
+    return '(' + " or ".join(tmp) + ')'
+
+def periodToQry( entity, per ):
+    ge  = '>='; le  = '<=';
+    wpq = []
+    if len(per) >= 1:
+       wpq.append(entity + ' ' + le + ' ' + str(per[0]))
+    if len(per) == 2:
+       wpq.append(entity + ' ' + ge + ' ' + str(per[1]))
+    return '(' + " and ".join(wpq) + ')'
+
+def getQryStr( uids, pids, wp, wpname, rp, fields ):
     """ 
     Generate the query string for gufi_query given constraints of
-     1. usrs (restricting to the list of users)
-     2. projs (restricting to the list of projs)
+     1. uids (restricting to the list of uids)
+     2. pids (restricting to the list of pids)
      3. wp (restricting to the period during 
             which files were last written)
      4. rp (restrict to the period during
             which files were last read)
     fields are the list of columns to be extracted
+    wpname can be 'mtime' (POSIX FS) or 'ctime' (HPSS)
     """ 
     sqt = '\''
-    dqt = '\"'
-    beg = 'SELECT ' + ','.join(fields) + ' FROM entries; '
+    beg = 'SELECT ' + ','.join(fields) + ' FROM entries '
     if bool(usrs) or bool(projs) or bool(wp) or bool(rp):
        totcons = []
-       if bool(usrs):
-          eachusrq = list(map(lambda x: 'uid = ' + dqt + x + dqt, usrs))
-          usrq = '(' + " or ".join(eachusrq) + ')'
-          totcons.append(usrq)
-       if bool(projs):
-          eachprojsq = list(map(lambda x: 'xattrs = ' + dqt + x + dqt, projs))
-          projsq = '(' + " or ".join(eachprojsq) + ')'
-          totcons.append(projsq)
+       if bool(uids):
+          totcons.append( usrProjPart( 'uid', uids ) )
+       if bool(pids):
+          totcons.append( usrProjPart( 'xattrs', pids ) )
+       if bool(wp):
+          totcons.append( periodToQry( wpname, wp ) )
+       if bool(rp):
+          totcons.append( periodToQry( 'atime', rp ) )
        totconsstr = "WHERE " + " and ".join(totcons)
-       return sqt + beg + totconsstr + sqt 
+       return sqt + beg + totconsstr + ';' + sqt
     else: 
-       return sqt + beg + sqt 
+       return sqt + beg + ';' + sqt
 
 
