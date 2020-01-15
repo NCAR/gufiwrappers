103e6df3367513f6856590a2b761bbf9ba724bde
Sidd Ghosh <siddsghosh@gmail.com>
2020-01-15 16:38:34 -0700

    Commiting to make a release

--- gplot.py
+++ gplot.py
@@ -20,13 +20,41 @@ def getData( histfile ):
     data = pd.read_csv( histfile )
     n = len(data['Year-mon'])
     d = np.arange(0,n)
-    y = []
+    yr = []
+    yw = []
+    yrw = []
     l = []
+#   dl = np.char.array( data['Year-mon'] )
+    dl = data['Year-mon']
     for usr in getAllUsers( data ):
-       y.append( np.array( data[usr + '_rw'] ) )
+       yr.append( np.array( data[usr + '_r'] )/1.e+12 )
+       yw.append( np.array( data[usr + '_w'] )/1.e+12 )
+       yrw.append( np.array( data[usr + '_rw'] )/1.e+12 )
        l.append( usr )
-    return d, y, l
+    return d, dl, l, yr, yw, yrw
 
+def plotRW( d, l, yrw ):
+    plt.stackplot( d, yrw[:10], labels = l )
+    plt.legend(loc='upper right')
+    plt.xlim(0,10)
+    plt.ylabel('TB')
+    plt.xlabel('Months')
+    plt.title('Read_time - write_time weighted by amount in TB')
+    plt.tight_layout(pad=0)
+    plt.show()
+
+def plotW( d, l, yw, dl ):
+    plt.stackplot( d, yw[:10], labels = l )
+    plt.legend(loc='upper left')
+    xmn = 576
+    xmx = 600
+    plt.xlim(xmn,xmx)
+    plt.ylabel('TB')
+    plt.xlabel('Yr-month')
+    plt.title('Data written over past 12 months')
+    plt.xticks(d[xmn:xmx:2],dl[xmn:xmx:2],rotation=25)
+    plt.tight_layout(pad=0)
+    plt.show()
 
 
 def parseCmdLine( ):
@@ -39,8 +67,10 @@ def parseCmdLine( ):
 
 if __name__ == "__main__":
     histfile = parseCmdLine( )
-    d, y, l = getData( histfile )
+    d, dl, l, yr, yw, yrw = getData( histfile )
 
-    plt.stackplot( d, y, labels = l )
-    plt.xlim(0,20)
-    plt.show()
+    plotRW( d, l, yrw )
+#   plotW( d, l, yr, dl )
+#   plt.stackplot( d, yrw, labels = l )
+#   plt.xlim(0,10)
+#   plt.show()
