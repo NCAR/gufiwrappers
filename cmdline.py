5ba2e133ae54d5be9aed3faf7914cb6cfb47a8a6
Sidd Ghosh <siddsghosh@gmail.com>
2020-05-11 15:50:24 -0600

    Added single minus as well, too many users are bumping onto this one

--- cmdline.py
+++ cmdline.py
@@ -165,37 +165,37 @@ def parserForQdh( ):
     global gufitmp
     parser = argparse.ArgumentParser(description='Query Data holding to generate filelist, report and plots',
              formatter_class=less_indent_formatter)
-    parser.add_argument('--gufitmp-dir=', dest='gufitmp', default=gufitmp,
+    parser.add_argument('-gufitmp-dir=','--gufitmp-dir=', dest='gufitmp', default=gufitmp,
                     metavar='path-name',
                     help="""Absolute path name to store the GUFI query output default:
 """ + gufitmp + """
 
 """)
-    parser.add_argument('--verbose','-v',dest='verbosity', 
+    parser.add_argument('-verbose','--verbose','-v',dest='verbosity', 
              action='store_true', help="""Adds verbosity
 
     """)
-    parser.add_argument('--storage=', dest='storage', nargs=1, 
+    parser.add_argument('-storage','--storage=', dest='storage', nargs=1, 
                        metavar='project,campaign,hpss',default='project',
              help="""Storage device where files are stored in.
   
     """)
-    parser.add_argument('--list=', dest='listd', nargs=1, required=False,
+    parser.add_argument('-list','--list=', dest='listd', nargs=1, required=False,
                        metavar='filename,size,owner,project,mtime,atime',
              help="""Generate list of files with one or more of the attributes
 from filename, size, owner, project, mtime, atime in order as
 specified delimited by comma(,).  
   
     """)
-    parser.add_argument('--filter-by-users=', dest='fuids', nargs='+', metavar='User1,User2',
+    parser.add_argument('-filter-by-users','--filter-by-users=', dest='fuids', nargs='+', metavar='User1,User2',
                     help="""Report only for User1[,User2]..
 
 """)
-    parser.add_argument('--filter-by-projects=', dest='fpids', nargs='+', metavar='Project1,Project2',
+    parser.add_argument('-filter-by-projects','--filter-by-projects=', dest='fpids', nargs='+', metavar='Project1,Project2',
                     help="""Report only for Project1[,Project2]..
 
 """)
-    parser.add_argument('--filter-by-write-period=', dest='writep', metavar='YYYY[MM[DD]]-YYYY[MM[DD]]',
+    parser.add_argument('-filter-by-write-period','--filter-by-write-period=', dest='writep', metavar='YYYY[MM[DD]]-YYYY[MM[DD]]',
              help="""for content written during the time window YYYY[MM[DD]]-YYYY[MM[DD]]
 either of begin or end period may be omitted for open interval. Here
 is how it will be interpreted:
@@ -207,7 +207,7 @@ For time specification 4-digit year is required (implies 01 month and
 2-digit day may be specified.
 
              """)
-    parser.add_argument('--filter-by-read-period=', dest='readp',  metavar='YYYY[MM[DD]]-YYYY[MM[DD]]',
+    parser.add_argument('-filter-by-read-period','--filter-by-read-period=', dest='readp',  metavar='YYYY[MM[DD]]-YYYY[MM[DD]]',
              help="""for content read during time the window YYYY[MM[DD]]-YYYY[MM[DD]]
 either of begin or end period may be omitted for open interval. Here
 is how it will be interpreted:
@@ -220,27 +220,27 @@ For time specification 4-digit year is required (implies 01 month and
 
              """)
     grprprtby = parser.add_mutually_exclusive_group(required=False)
-    grprprtby.add_argument('--report-by-users', dest='byusers', action='store_true',
+    grprprtby.add_argument('-report-by-users','--report-by-users', dest='byusers', action='store_true',
                        help="""report by user-name / user-ids (if mapping not found)
 
 """)
-    grprprtby.add_argument('--report-by-projects', dest='byprojects', action='store_true',
+    grprprtby.add_argument('-report-by-projects','--report-by-projects', dest='byprojects', action='store_true',
                        help="""report by project-name / project-id (if mapping not found) HPSS only
 
 """)
-    grprprtby.add_argument('--report-by-subdirs', dest='bysubdirs',  action='store_true',
+    grprprtby.add_argument('-report-by-subdirs','--report-by-subdirs', dest='bysubdirs',  action='store_true',
                        help="""report by subdirectories of this parent directory
 
 """)
-    parser.add_argument('--nthreads=', dest='ncores', default=40, metavar='number-of-cores or processes',
+    parser.add_argument('-nthreads=','--nthreads=', dest='ncores', default=40, metavar='number-of-cores or processes',
                       help="""Number of cores / threads to run
 
 """)
-    parser.add_argument('--nsbins=', dest='nsbins', default=8, metavar='number-of-histogram bins [8]',
+    parser.add_argument('-nsbins','--nsbins=', dest='nsbins', default=8, metavar='number-of-histogram bins [8]',
                        help="""Number of write / read stat histogram bins in report
 
 """)
-    parser.add_argument('--directory=', dest='treename', metavar='directory name to query',
+    parser.add_argument('-directory','--directory=', dest='treename', metavar='directory name to query',
     help="""Absolute path of the filesystem tree located \
 either in glade, campaign or HPSS
 
